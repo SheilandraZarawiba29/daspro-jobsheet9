05c08700a33aa89740d6a29610a6393147324198
SheilandraZarawiba29 <Sheilandrazarawiba561@gmail.com>
2024-10-31 15:04:52 +0700

    Pertanyaan 3 percobaan 2

--- ArrayNilai28.java
+++ ArrayNilai28.java
@@ -5,13 +5,15 @@ public class ArrayNilai28 {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < nilaiAkhir.length; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.print("Massukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
         }
     }
 }
